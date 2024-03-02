03869ed502f39085dc874deb64a56fb2265a9ab5
anas12421 <anas161090@gmail.com>
2024-03-02 12:24:22 +0600

    demo

--- js/custom.js
+++ js/custom.js
@@ -17,9 +17,9 @@ $(function () {
     }
 
     if (scrolling > 50) {
-      $(".main_nav").addClass("nav_ex")
+      $(".main-navbar").addClass("nav_ex")
     } else {
-      $(".main_nav").removeClass("nav_ex")
+      $(".main-navbar").removeClass("nav_ex")
     }
   })
 
@@ -33,6 +33,74 @@ $(function () {
   new WOW().init();
 
 
+
+  // banner part slider js
+
+  $('.banner_main_slider').slick({
+    fade:true,
+    arrows:false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+
+  // Client part slider js
+
+  
+$('.client_main_slider').slick({
+  arrows:false,
+  speed: 600,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  centerMode: true,
+  centerPadding: '0',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
 
 
