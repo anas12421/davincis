3e31fd099418cade525f09f44ff3dd67bf008cf4
anas12421 <anas161090@gmail.com>
2024-03-03 21:02:26 +0600

    demo

--- js/custom.js
+++ js/custom.js
@@ -37,7 +37,7 @@ $(function () {
 
   // full body wow js
   new WOW().init();
-  
+
   // banner part slider js
   $('.banner_main_slider').slick({
     fade: true,
@@ -100,6 +100,32 @@ $(function () {
     ]
   });
 
+  // Team part slider js
+  $('.team_main_slider').slick({
+    arrows: false,
+    speed: 600,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
 
+        }
+      },
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false
+        }
+      }
+    ]
+  });
 
 })
