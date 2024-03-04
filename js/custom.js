30ab0de628c91aaf8bef5465f3f60c97821ac6d7
anas12421 <anas161090@gmail.com>
2024-03-04 21:04:14 +0600

    demo

--- js/custom.js
+++ js/custom.js
@@ -38,29 +38,6 @@ $(function () {
   // full body wow js
   new WOW().init();
 
-  // banner part slider js
-  $('.banner_main_slider').slick({
-    fade: true,
-    arrows: false,
-    speed: 300,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1500,
-    responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
-
   // Client part slider js
   $('.client_main_slider').slick({
     arrows: false,
@@ -105,6 +82,34 @@ $(function () {
     arrows: false,
     speed: 600,
     slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+
+        }
+      },
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false
+        }
+      }
+    ]
+  });
+
+  // Latest Blog part slider js
+  $('.latest_blog_main_slider').slick({
+    arrows: false,
+    speed: 600,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 1500,
